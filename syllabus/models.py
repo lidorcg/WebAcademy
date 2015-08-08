fd8fadb121d16dc45be04f41a7fe7dfb25253b51
lidorcg <lidorcg@gmail.com>
2015-08-08 16:45:50 +0300

    -Added progression tracking and time

--- syllabus/models.py
+++ syllabus/models.py
@@ -4,8 +4,9 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    # ToDo add progress tracking
+    description = models.TextField(blank=True)
+    prerequisites = models.TextField(blank=True)
+    requirements = models.TextField(blank=True)
     # ToDo add creator relationship to User
 
     def __str__(self):
@@ -14,13 +15,20 @@ class Course(models.Model):
     def get_modules(self):
         return self.module_set.order_by('order')
 
+    def get_progress(self):
+        if self.module_set.count() == 0:
+            return 1
+        return sum(m.get_progress() for m in self.module_set.all()) / self.module_set.count()
+
+    def get_time(self):
+        return sum(m.get_time() for m in self.module_set.all())
+
 
 class Module(models.Model):
     course = models.ForeignKey('Course')
     title = models.CharField(max_length=100)
     description = models.TextField()
-    order = models.PositiveSmallIntegerField()
-    # ToDo add progress tracking
+    order = models.PositiveSmallIntegerField(unique=True)
 
     def __str__(self):
         return self.title
@@ -28,16 +36,25 @@ class Module(models.Model):
     def get_contents(self):
         return self.content_set.order_by('order')
 
+    def get_progress(self):
+        if self.content_set.count() == 0:
+            return 1
+        return self.content_set.filter(done=True).count() / self.content_set.count()
+
+    def get_time(self):
+        return sum(c.time for c in self.content_set.all())
+
 
 class Content(models.Model):
     module = models.ForeignKey('Module')
     type = models.ForeignKey('Type')
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    time = models.DurationField()  # ToDo duration in minutes
+    description = models.TextField(blank=True)
+    time = models.DurationField()
     concepts = models.ManyToManyField('Concept', through='ContentConcept')
-    order = models.PositiveSmallIntegerField()
-    # ToDo add progress tracking
+    order = models.PositiveSmallIntegerField(unique=True)
+    requirements = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -45,6 +62,7 @@ class Content(models.Model):
 
 class Concept(models.Model):
     name = models.CharField(max_length=100)
+    covered = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
