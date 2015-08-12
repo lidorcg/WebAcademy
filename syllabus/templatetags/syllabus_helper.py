c459f8a2fa4a877bbc403af7792dff820ad457a0
lidorcg <lidorcg@gmail.com>
2015-08-12 10:13:04 +0300

    - finished update form functionality

--- syllabus/templatetags/syllabus_helper.py
+++ syllabus/templatetags/syllabus_helper.py
@@ -10,4 +10,9 @@ def multiply(a, b):
 
 @register.filter
 def to_hours(timedelta):
-    return timedelta.days*24 + timedelta.seconds / 3600
+    return timedelta.days * 24 + timedelta.seconds / 3600
+
+
+@register.simple_tag()
+def append(a, b):
+    return a + str(b)
