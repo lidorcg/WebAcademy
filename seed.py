9de0f7bddfab7ce3879d90cb3de7986f08b291ed
lidorcg <lidorcg@gmail.com>
2015-08-26 11:34:10 +0300

    - fixed db seed

--- seed.py
+++ seed.py
@@ -22,13 +22,15 @@ UnitType(name='File', icon='insert_drive_file').save()
 from syllabus.models import Course, Module, Lesson, Unit
 
 c = Course(title='Demo',
-           description='This is a sample course to show all the options this system holds.').save()
+           description='This is a sample course to show all the options this system holds.')
+c.save()
 
 for i in range(5):
     m = Module(course=c,
                order=i,
                title='M' + str(i),
-               description='This is a module(kind of a chapter) in the course.').save()
+               description='This is a module(kind of a chapter) in the course.')
+    m.save()
 
     for j in range(5):
         l = Lesson(module=m,
@@ -38,7 +40,8 @@ for i in range(5):
                    A lesson may introduce to the module, deliver one main subject or a few small related subjects,
                    practice the learned subjects, present a test or summarize the module.""",
                    type=LessonType.objects.get(j % LessonType.objects.count()),
-                   time=timedelta(hours=random.randint(0, 3), minutes=random.randint(0, 60))).save()
+                   time=timedelta(hours=random.randint(0, 3), minutes=random.randint(0, 60)))
+        l.save()
 
         for k in range(5):
             u = Unit(lesson=l,
