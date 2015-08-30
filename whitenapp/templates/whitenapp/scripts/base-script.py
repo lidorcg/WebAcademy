fc856bea21ade8d4f77b7a1d5ddb84a7c49da8ae
lidorcg <lidorcg@gmail.com>
2015-08-30 17:51:02 +0300

    - added videos and normal files download

--- whitenapp/templates/whitenapp/scripts/base-script.py
+++ whitenapp/templates/whitenapp/scripts/base-script.py
@@ -1,10 +1,17 @@
 import os
+import urllib.request
+if not os.path.exists("pafy.py"):
+	url = 'https://raw.githubusercontent.com/np1/pafy/master/pafy/pafy.py'
+	print("downloading pafy.py...")
+	urllib.request.urlretrieve(url, "pafy.py")
+import pafy
+
 {% if course %}
 {% include 'whitenapp/scripts/course-script.py' with c=course %}
 {% elif module %}
 {% include 'whitenapp/scripts/module-script.py' with m=module %}
 {% elif lesson %}
-{% include 'whitenapp/scripts/module-script.py' with l=lesson %}
+{% include 'whitenapp/scripts/lesson-script.py' with l=lesson %}
 {% elif unit %}
-{% include 'whitenapp/scripts/module-script.py' with u=unit %}
+{% include 'whitenapp/scripts/unit-script.py' with u=unit %}
 {% endif %}
