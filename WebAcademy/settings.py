b5e500105819abb4cd0c2bc6b29048f0eb44359a
lidorcg <lidorcg@gmail.com>
2015-09-01 21:25:28 +0300

    - added reminder to fix reorder to work with csrf security

--- WebAcademy/settings.py
+++ WebAcademy/settings.py
@@ -111,6 +111,16 @@ STATICFILES_FINDERS = (
 )
 
 # production settings
+# ToDo search for a solution for CSRF_COOKIE_SECURE and reorder to work
+# SECURE_HSTS_SECONDS = 31536000
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_HTTPONLY = True
+# X_FRAME_OPTIONS = 'DENY'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
