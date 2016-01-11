f03a318fa7882a718560d8ba9d028f0f1230725c
lidorcg <lidorcg@gmail.com>
2016-01-11 11:15:07 +0200

    change: to synced page

--- wizard/static/wizard/js/step-2.js
+++ wizard/static/wizard/js/step-2.js
@@ -2,12 +2,9 @@ $(document).ready(function () {
     $('#add-tag').click(function () {
         var name = this.form.elements["name"].value;
         var tag = $('<span class="tag btn btn-default"></span>').text(name);
-        $('.all-tags').append(tag);
-    });
-
-    $('#go-to-3').on('click', function (event) {
-        $('.nav-pills a[href="#step-3"]').tab('show');
+        $('#all-tags').append(tag);
     });
 });
+// ToDo use ajax and success func to add tags
 // ToDo reset form after submit
 // ToDo add form validations
